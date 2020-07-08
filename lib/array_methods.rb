b950ba3930c87babcf16e6c7329e1ae2e989eeea
John Easter <john@owltestprep.com>
2020-07-08 02:34:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,20 +2,20 @@ def using_include(array = ["Mom", "Dad", "John", "Seth"], element = "John")
   array.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort(array = ["Mom", "Dad", "John", "Seth"])
+  array.sort
 end
 
-def using_reverse(array)
-
+def using_reverse(array = ["Mom", "Dad", "John", "Seth"])
+  array.reverse
 end
 
-def using_first(array)
-
+def using_first(array = ["Mom", "Dad", "John", "Seth"])
+  array.first
 end
 
-def using_last(array)
-
+def using_last(array = ["Mom", "Dad", "John", "Seth"])
+  array.last
 end
 
 def using_size(array)
